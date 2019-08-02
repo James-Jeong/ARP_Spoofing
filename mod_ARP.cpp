25a7cd42c2e5cca40e6bf9d477a87bc4f9141bc6
jamesj <ehddnr177@naver.com>
2019-08-02 23:49:48 +0900

    Modify mod_ARP

--- mod_ARP.cpp
+++ mod_ARP.cpp
@@ -1,29 +1,36 @@
 #include "stdafx.h"
 
-ARP_header::ARP_header(){
-	this->frame_type = htons(ARP_FRAME_TYPE);
-	this->mac_type = htons(ETHER_MAC_TYPE);
-	this->prot_type = htons(IP_PROTO_TYPE);
-	this->mac_addr_size = ETH_MAC_ADDR_LEN;
-	this->prot_addr_size = IP_ADDR_LEN;
-	this->op = htons(OP_ARP_REQUEST);
-}
+/*
+this->mac_type = htons(ETHER_MAC_TYPE);
+this->prot_type = htons(IP_PROTO_TYPE);
+this->mac_addr_size = ETH_MAC_ADDR_LEN;
+this->prot_addr_size = IP_ADDR_LEN;
+this->op = htons(OP_ARP_REPLY);
+*/
 
-void ARP_header::Print_ARP(){
+/*
+this->mac_type = htons(ETHER_MAC_TYPE);
+this->prot_type = htons(IP_PROTO_TYPE);
+this->mac_addr_size = ETH_MAC_ADDR_LEN;
+this->prot_addr_size = IP_ADDR_LEN;
+this->op = htons(OP_ARP_REQUEST);
+*/
+
+void Print_ARP(struct ARP_header* a){
 	int count = 0;
 	printf("Destination MAC = ");
 	for(int i = 0; i < ETH_MAC_ADDR_LEN; i++){
-		printf("%02x:", this->Destination_mac_addr[i]);
+		printf("%02x:", a->Destination_mac_addr[i]);
 	}
 	printf("\n");
 	printf("Source MAC = ");
 	for(int i = 0; i < ETH_MAC_ADDR_LEN; i++){
-		printf("%02x:", this->src_mac_addr[i]);
+		printf("%02x:", a->src_mac_addr[i]);
 	}
 	printf("\n");
 	printf("Sender MAC = ");
 	for(int i = 0; i < ETH_MAC_ADDR_LEN; i++){
-		printf("%02x:", this->sender_mac_addr[i]);
+		printf("%02x:", a->sender_mac_addr[i]);
 	}
 	printf("\n");
 	printf("Sender IP = ");
@@ -32,13 +39,13 @@ void ARP_header::Print_ARP(){
 			printf(".");
 			count = 0;
 		}
-		printf("%d", this->sender_ip_addr[i]);
+		printf("%d", a->sender_ip_addr[i]);
 		count++;
 	}
 	printf("\n");
 	printf("Target MAC = ");
 	for(int i = 0; i < ETH_MAC_ADDR_LEN; i++){
-		printf("%02x:", this->target_mac_addr[i]);
+		printf("%02x:", a->target_mac_addr[i]);
 	}
 	printf("\n");
 	printf("Target IP = ");
@@ -48,19 +55,19 @@ void ARP_header::Print_ARP(){
 			printf(".");
 			count = 0;
 		}
-		printf("%0d", this->target_ip_addr[i]);
+		printf("%0d", a->target_ip_addr[i]);
 		count++;
 	}
 	printf("\n\n");
 }
 
 
-void ARP_header::die(char* str){
+void EndOfProgram(char* str){
 	fprintf(stderr, "%s\n", str);
 	exit(1);
 }
 
-void ARP_header::tomar_ip_addr(struct in_addr* in_addr, char* str){
+void tomar_ip_addr(struct in_addr* in_addr, char* str){
 	struct hostent* hostp;
 
 	in_addr->s_addr = inet_addr(str);
@@ -75,29 +82,29 @@ void ARP_header::tomar_ip_addr(struct in_addr* in_addr, char* str){
 	}
 }
 
-void ARP_header::tomar_mac_addr(u_char* buf, char* str){
+void tomar_mac_addr(u_char* buf, char* str){
 	u_char c, val;
 
 	for(int i = 0; i < ETH_MAC_ADDR_LEN; i++){
 		//printf("%s\n", str);
 		if(!(c = tolower(*str++))){
 			//printf("%s\n", str);
-			die("Invalid mac addr");
+			EndOfProgram("Wrong mac addr");
 		}
 		if(isdigit(c)) val = c - '0';
 		else if(c >= 'a' && c <= 'f') val = c - 'a' + 10;
-		else die("Invalid mac addr");
+		else EndOfProgram("Wrong mac addr");
 
 		*buf = val << 4;
 		if(!(c = tolower(*str++))){
 			//printf("%s\n", str);
-			die("Invalid mac addr");
+			EndOfProgram("Wrong mac addr");
 		}
 		if(isdigit(c)) val = c - '0';
 		else if(c >= 'a' && c <= 'f') val = c - 'a' + 10;
 		else{
 			//printf("%s\n", str);
-			die("Invalid mac addr");
+			EndOfProgram("Wrong mac addr");
 		}
 
 		*buf++ |= val;
@@ -106,6 +113,34 @@ void ARP_header::tomar_mac_addr(u_char* buf, char* str){
 	}
 }
 
-
+void check_ARP(const u_char* Packet_DATA){
+	struct ARP_header* ah = (struct ARP_header*)(Packet_DATA);
+	if((int)htons(ah->prot_type) == 2048){
+		//printf("ah->mac_type : %x\n", htons(ah->mac_type));
+		printf("ah->prot_type : %d\n", htons(ah->prot_type));
+		//printf("ah->mac_addr_size : %x\n", htons(ah->mac_addr_size));
+		//printf("ah->prot_addr_size : %x\n", htons(ah->prot_addr_size));
+		printf("ah->op : %d\n", htons(ah->op));
+		//printf("ah->sender_ip_addr : %d\n", ah->sender_ip_addr[0]);
+		//printf("ah->sender_ip_addr : %d\n", ah->sender_ip_addr[1]);
+		//printf("ah->sender_ip_addr : %d\n", ah->sender_ip_addr[2]);
+		printf("ah->sender_ip_addr : %d\n", ah->sender_ip_addr[3]);
+	}
+	else { printf("Packet is not ARP\n"); return ; }
+	
+	if((int)htons(ah->op) != 2){
+		printf("ARP packet is not Reply\n");
+		return ; // reply : 2
+	}
+	else{
+		printf("ah->sender_mac_addr : %x\n", ah->sender_mac_addr[0]);
+		printf("ah->sender_mac_addr : %x\n", ah->sender_mac_addr[1]);
+		printf("ah->sender_mac_addr : %x\n", ah->sender_mac_addr[2]);
+		printf("ah->sender_mac_addr : %x\n", ah->sender_mac_addr[3]);
+		printf("ah->sender_mac_addr : %x\n", ah->sender_mac_addr[4]);
+		printf("ah->sender_mac_addr : %x\n", ah->sender_mac_addr[5]);
+	}
+	//return ah->sender_mac_addr;
+}
 
 
--- mod_ARP.h
+++ mod_ARP.h
@@ -6,33 +6,39 @@
 #define ETH_MAC_ADDR_LEN 6
 #define IP_ADDR_LEN 4
 #define ETHER_MAC_TYPE 1
-#define OP_ARP_REQUEST 2
+#define OP_ARP_REPLY 2
+#define OP_ARP_REQUEST 1
 #define ARP_FRAME_TYPE 0x0806
 #define IP_PROTO_TYPE 0x0800
 #define DEFAULT_DEVICE "enp0s3"
 
-class ARP_header{
-	private:
-	public:
-		u_char Destination_mac_addr[ETH_MAC_ADDR_LEN];
-		u_char src_mac_addr[ETH_MAC_ADDR_LEN];
-		u_short frame_type;
-		u_short mac_type;
-		u_short prot_type;
-		u_char mac_addr_size;
-		u_char prot_addr_size;
-		u_short op;
-		u_char sender_mac_addr[ETH_MAC_ADDR_LEN];
-		u_char sender_ip_addr[IP_ADDR_LEN];
-		u_char target_mac_addr[ETH_MAC_ADDR_LEN];
-		u_char target_ip_addr[IP_ADDR_LEN];
-		u_char padding[18];
+// Destination_mac_addr : victim's mac address or gateway's mac address
+// src_mac_addr : attacker's mac address
+// sender_mac_addr : attacker's mac address
+// sender_ip_addr : attacker's ip address
+// target_mac_addr : victim's mac address
+// target_ip_addr : victim's ip address
 
-		ARP_header();
-		void Print_ARP();
-		void die(char* str);
-		void tomar_ip_addr(struct in_addr* sender, char* str);
-		void tomar_mac_addr(u_char* buf, char* str);
+struct ARP_header{
+	u_short mac_type;
+	u_short prot_type;
+	u_char mac_addr_size;
+	u_char prot_addr_size;
+	u_short op;
+	//u_short frame_type;
+	u_char Destination_mac_addr[ETH_MAC_ADDR_LEN];
+	u_char src_mac_addr[ETH_MAC_ADDR_LEN];
+	u_char sender_mac_addr[ETH_MAC_ADDR_LEN];
+	u_char sender_ip_addr[IP_ADDR_LEN];
+	u_char target_mac_addr[ETH_MAC_ADDR_LEN];
+	u_char target_ip_addr[IP_ADDR_LEN];
+	u_char padding[18];
 };
+
+void Print_ARP(struct ARP_header* a);
+void EndOfProgram(char* str);
+void tomar_ip_addr(struct in_addr* sender, char* str);
+void tomar_mac_addr(u_char* buf, char* str);
+void check_ARP(const u_char* Packet_DATA);
 
 #endif
