e039930bf9349d636fdc57cf752c105eb35990f7
jamesj <ehddnr177@naver.com>
2019-08-09 00:54:24 +0900

    Modify mod_Eth.cpp

--- mod_Eth.cpp
+++ mod_Eth.cpp
@@ -5,13 +5,16 @@ int Ethernet_header::Check_Eth(const u_char* Packet_DATA, char* sender_mac, char
 	struct libnet_ethernet_hdr* EH = (struct libnet_ethernet_hdr*)(Packet_DATA);
 	u_short ethernet_type;
 	ethernet_type = ntohs(EH->ether_type);
-	if(ethernet_type != 0x0800){
-		printf("Ethernet type is not IP\n");
+	if(ethernet_type == 0x0800){
+		printf("Ethernet type is IP\n");
+	}
+	else if(ethernet_type == 0x0806){
+		printf("Ethernet type is ARP\n");
+	}
+	else{
+		printf("Warning: Unknown Ethernet type!\n");
 		return 0;
-	} // IP
-
-	printf("sender mac : %s\n", sender_mac);
-	printf("target mac : %s\n", target_mac);
+	}
 
 	char* EH_smac = (char*)malloc(sizeof(sender_mac));
 	if(EH_smac == NULL){ perror("EH_smac malloc error"); exit(1); }
@@ -33,8 +36,11 @@ int Ethernet_header::Check_Eth(const u_char* Packet_DATA, char* sender_mac, char
 		EH->ether_dhost[4],
 		EH->ether_dhost[5]);
 
-	printf("EH_smac : %s\n", EH_smac);
-	printf("EH_dmac : %s\n", EH_dmac);
+	//printf("EH_smac : %s\n", EH_smac);
+	//printf("EH_dmac : %s\n", EH_dmac);
+	printf("sender mac : %s\n", sender_mac);
+	//printf("target mac : %s\n", target_mac);
+	printf("attacker_mac : %s\n", my_mac);
 
 	// Is sender mac address equal to EH's mac address?
 	if(strncmp(sender_mac, EH_smac, strlen(sender_mac)) == 0){
@@ -43,26 +49,22 @@ int Ethernet_header::Check_Eth(const u_char* Packet_DATA, char* sender_mac, char
 	if(strncmp(my_mac, EH_dmac, strlen(my_mac)) == 0){
 		sum += 1;
 	}
+	else if(strncmp(EH_dmac, "ffffffffffff", strlen(EH_dmac))){
+		sum += 2;
+	}
+	else if(strncmp(EH_dmac, target_mac, strlen(EH_dmac))){
+		sum += 3;
+	}
 	return sum;
-} // if return 2, Success to find spoofed packet
+}
+// if return 2, Success to find spoofed packet
+// else if return 3, Success to find broadcast packet sended by sender
 
 uint8_t Ethernet_header::Print_Eth(const u_char* Packet_DATA){
 	struct libnet_ethernet_hdr* EH = (struct libnet_ethernet_hdr*)(Packet_DATA);
 	uint8_t EH_length = (uint8_t)(sizeof(EH));
 	u_short ethernet_type;
 	ethernet_type = ntohs(EH->ether_type);
-
-	if(ethernet_type == 0x0800){
-		printf("Ethernet type is IP\n");
-	} // IP CHECK
-	else if(ethernet_type == 0x0806){
-		printf("Ethernet type is ARP\n");
-	}
-	else{
-		printf("Ethernet type is Unknown\n");
-		return 0;
-	}
-
 	printf("[Source] <MAC> Address : %02x:%02x:%02x:%02x:%02x:%02x:\n",
 		EH->ether_shost[0],
 		EH->ether_shost[1],
