536ad1cffadb60f7b2447567676885031d8bc71f
jamesj <ehddnr177@naver.com>
2019-08-10 02:08:04 +0900

    Modify main

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int num_of_parameter = 0;
 pid_t pid[SESSION_NUM];
-pcap_t* handle;
+pcap_t* handle[SESSION_NUM];
 
 void terminate_Process(int sig){
 	for(int i = 0; i < num_of_parameter; i++){
 		kill(pid[i], SIGKILL);
 		pthread_join(thread[i], NULL);
 	}
-	pcap_close(handle);
+	for(int j = 0; j < num_of_parameter; j++){
+		pcap_close(handle[j]);
+	}
 }
 
 char* make_Parameter_REQ(int num, char* at_ip_addr, char* at_mac_addr, char* ip, pcap_t* handles){
@@ -67,22 +69,67 @@ int main(int argc, char* argv[]) {
 	}
 
 // ########## Make a pcap environment ##########
-	char errbuf[PCAP_ERRBUF_SIZE];
-	// P Mode
-	handle = pcap_open_live(DEFAULT_DEVICE, BUFSIZ, 1, 1000, errbuf);
-	if (handle == NULL) {
-		fprintf(stderr, "couldn't open device %s: %s\n", DEFAULT_DEVICE, errbuf);
-		return -1;
+	struct bpf_program fp;
+	char errbuf[PCAP_ERRBUF_SIZE]; int ret = 0;
+	char* dev = pcap_lookupdev(errbuf); char* net; char* mask;
+	bpf_u_int32 netp; bpf_u_int32 maskp;
+	struct pcap_pkthdr hdr; struct in_addr net_addr, mask_addr;
+	int cnt = 2;
+
+	if(dev == NULL) { printf("%s\n", errbuf); exit(1); }
+	printf("Dev : %s\n", dev);
+
+	ret = pcap_lookupnet(dev, &netp, &maskp, errbuf);
+	if(ret == -1) { printf("%s\n", errbuf); exit(1); }
+
+	net_addr.s_addr = netp;
+	net = inet_ntoa(net_addr);
+	printf("Net : %s\n", net);
+
+	mask_addr.s_addr = maskp;
+	mask = inet_ntoa(mask_addr);
+	printf("Mask : %s\n\n", mask);
+
+	char* src_str[num_of_parameter];
+	for(int i = 0; i < num_of_parameter; i++){
+		src_str[i] = (char*)malloc(sizeof(20));
+		strncpy(src_str[i], "src ", 4);
+		strncat(src_str[i], argv[cnt], strlen(argv[cnt]));
+		cnt += 2;
 	}
+
+	cnt = 0;
+	// P Mode
+	for(int i = 0; i < num_of_parameter; i++){
+		handle[i] = pcap_open_live(dev, BUFSIZ, PROMISCUOUS_MODE, 1000, errbuf);
+		if (handle[i] == NULL) {
+			fprintf(stderr, "handle[%d] couldn't open device %s: %s\n", i, dev, errbuf);
+			return -1;
+		}
+		// 3->filter rule
+		if(pcap_compile(handle[i], &fp, src_str[i], 0, netp) == -1){
+			printf("Compile ERROR!\n");
+			exit(1);
+		}
+		if(pcap_setfilter(handle[i], &fp) == -1){
+			printf("Setfilter ERROR!\n");
+			exit(1);
+		}
+		cnt += 2;
+	}
+	cnt = 2;
 // ########################################
 
 // ########## Make a socket to find my IP & MAC address ##########
-	char* a_mac_addr = (char*)malloc(128);
+	struct Info_mymac* IM = (struct Info_mymac*)malloc(sizeof(struct Info_mymac));
+	char* a_mac_addr = (char*)malloc(20);
 	if(a_mac_addr == NULL){ perror("a_mac_addr malloc error"); exit(1); }
-	char* s_ip_addr = (char*)malloc(128);
+	char* s_ip_addr = (char*)malloc(20);
 	if(s_ip_addr == NULL){ perror("s_ip_addr malloc error"); exit(1); }
-	pthread_create(&thread[0], NULL, find_My_Mac, (void*)(a_mac_addr));
-	pthread_join(thread[0], (void**)(&s_ip_addr));
+	IM = find_My_Mac();
+	strncpy(a_mac_addr, IM->my_mac, strlen(IM->my_mac));
+	strncpy(s_ip_addr, IM->my_ip, strlen(IM->my_ip));
+	a_mac_addr[12] = '\0';
 	printf("[ Success to find my mac address : %s ]\n", a_mac_addr);
 // ########################################
 
@@ -99,7 +146,13 @@ int main(int argc, char* argv[]) {
 	char* dmac[num_of_parameter];
 
 	Session s[SESSION_NUM];
-	int cnt = 2;
+	char eb[PCAP_ERRBUF_SIZE];
+	pcap_t* h = pcap_open_live(dev, BUFSIZ, PROMISCUOUS_MODE, 1000, eb);
+	if (h == NULL) {
+		fprintf(stderr, "h couldn't open device %s: %s\n", dev, eb);
+		return -1;
+	}
+		
 	printf("[ NUM_OF_PRARMETER : %d ]\n", num_of_parameter);
 	for(int i = 0; i < num_of_parameter; i++){
 		// Find mac using pcap_sendpacket
@@ -107,7 +160,7 @@ int main(int argc, char* argv[]) {
 		if(smac[i] == NULL){ perror("smac malloc error"); exit(1); }
 		char* t_smac = (char*)malloc(sizeof(char) * 40);
 		if(t_smac == NULL){ perror("t_mac malloc error"); exit(1); }
-		t_smac = make_Parameter_REQ(i+1, s_ip_addr, a_mac_addr, argv[cnt], handle);
+		t_smac = make_Parameter_REQ(i+1, s_ip_addr, a_mac_addr, argv[cnt], h);
 		smac[i] = t_smac;
 		printf("[ < %d > Success to find < %s > mac address : %s ]\n", i+1, argv[cnt], smac[i]);
 
@@ -115,19 +168,20 @@ int main(int argc, char* argv[]) {
 		if(dmac[i] == NULL){ perror("dmac malloc error"); exit(1); }	
 		char* t_dmac = (char*)malloc(sizeof(char) * 40);
 		if(t_dmac == NULL){ perror("t_mac malloc error"); exit(1); }
-		t_dmac = make_Parameter_REQ(i+1, s_ip_addr, a_mac_addr, argv[cnt+1], handle);
+		t_dmac = make_Parameter_REQ(i+1, s_ip_addr, a_mac_addr, argv[cnt+1], h);
 		dmac[i] = t_dmac;
 		printf("[ < %d > Success to find < %s > mac address : %s ]\n", i+1, argv[cnt+1], dmac[i]);
 		printf("%d smac : %s\n", i, smac[i]);
 		printf("%d dmac : %s\n", i, dmac[i]);
 		cnt += 2;
 	}
+	pcap_close(h);
 	
 	cnt = 2;
 	struct Parameter_Pthread* pt[num_of_parameter];
 	for(int i = 0; i < num_of_parameter; i++){
 		pt[i] = (struct Parameter_Pthread*)malloc(sizeof(struct Parameter_Pthread));
-		// ########## Make contents of Attack parameters ##########	
+		// ########## Make contents of Attack parameters ##########
 		pt[i]->sip = (char*)malloc(strlen(argv[cnt]));
 		if(pt[i]->sip == NULL){ perror("pt->sip malloc error"); exit(1); }
 		pt[i]->smac = (char*)malloc(strlen(a_mac_addr));
@@ -140,17 +194,21 @@ int main(int argc, char* argv[]) {
 		strncpy(pt[i]->smac, a_mac_addr, strlen(a_mac_addr));
 		strncpy(pt[i]->tip, argv[cnt], strlen(argv[cnt]));
 		strncpy(pt[i]->tmac, smac[i], strlen(smac[i]));
-		pt[i]->handle = handle;
+		pt[i]->handle = handle[i];
 		pt[i]->session_Number = i+1;
 		// ########################################
 
 		// ########## Attack ##########
-		s[i].set(i+1, smac[i], argv[cnt], dmac[i], argv[cnt+1], handle, a_mac_addr, pt[i]);
+		s[i].set(i+1, smac[i], argv[cnt], dmac[i], argv[cnt+1], handle[i], a_mac_addr, pt[i]);
 		// ########################################
 		cnt += 2;
 	}
+
+	//for(int z = 0; z < num_of_parameter; z++){
+	//	pcap_loop(handle[z], 0, s[z].handle_session, NULL);
+	//}	
 	while(1){
-		for(int i = 0; i < num_of_parameter; i++)
+		for(int i = 0; i < num_of_parameter; i++)		
 			s[i].handle_session();
 	}
 // @@@@@@@@@@ @@@@@@@@@@ @@@@@@@@@@ @@@@@@@@@@
